d0cf15dd8b7fed69c2882bb33a57f2954d432f02
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-06-19 22:41:30 +0700

    refactor: [EMS-58] remove UserFilter component and integrate filtering directly in UserTable

--- src/pages/User/UserManagement/components/index.ts
+++ src/pages/User/UserManagement/components/index.ts
@@ -1,5 +1,4 @@
-import UserFilter from './UserFilter/UserFilter';
 import UserModal from './UserModal/UserModal';
 import UserTable from './UserTable/UserTable';
 
-export { UserFilter, UserModal, UserTable };
+export { UserModal, UserTable };

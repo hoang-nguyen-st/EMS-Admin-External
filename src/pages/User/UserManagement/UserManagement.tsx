177b18102179b9caa6ce0e56ac2273a7f2f4d40d
Hoang Nguyen <nguyenlamhoang7146@gmail.com>
2025-06-19 16:58:25 +0700

    refactor: [EMS-55] clean up user interface definitions and improve type usage

--- src/pages/User/UserManagement/UserManagement.tsx
+++ src/pages/User/UserManagement/UserManagement.tsx
@@ -5,13 +5,13 @@ import { useTranslation } from 'react-i18next';
 import { UserTable, UserFilter, UserModal } from './components';
 import { UserStatus } from '@app/constants';
 import { useGetUsers } from '@app/hooks';
-import { GetUsersParams } from '@app/interface/user.interface';
+import { GetUsersParams, UserColumns } from '@app/interface/user.interface';
 import './UserManagement.scss';
 
 const UserManagement = () => {
   const { t } = useTranslation();
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [selectedUser, setSelectedUser] = useState<any>(null);
+  const [selectedUser, setSelectedUser] = useState<UserColumns | null>(null);
   const [filters, setFilters] = useState<GetUsersParams>({
     search: '',
     status: UserStatus.DEFAULT,
@@ -21,12 +21,12 @@ const UserManagement = () => {
 
   const { data, refetch } = useGetUsers(filters);
 
-  const handleAddUser = (record: any) => {
+  const handleAddUser = (record: UserColumns) => {
     setSelectedUser(record);
     setIsModalVisible(true);
   };
 
-  const handleEditUser = (record: any) => {
+  const handleEditUser = (record: UserColumns) => {
     setSelectedUser(record);
     setIsModalVisible(true);
   };
@@ -58,15 +58,12 @@ const UserManagement = () => {
   const handleStatusChange = (value: UserStatus) => {
     setFilters((prev) => ({
       ...prev,
-      status: value,
+      status: value ? value : UserStatus.DEFAULT,
       page: 1,
     }));
   };
 
-  const handleDateChange = (
-    dates: [Dayjs | null, Dayjs | null] | null,
-    dateStrings: [string, string],
-  ) => {
+  const handleDateChange = (dates: [Dayjs | null, Dayjs | null] | null) => {
     setFilters((prev) => ({
       ...prev,
       startDate: dates && dates[0] ? dates[0].format('YYYY-MM-DD') : '',
